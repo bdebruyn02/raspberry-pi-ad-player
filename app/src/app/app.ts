2a074c31aff4fcec1552e99b4e3ebd0ca2754db9
Brandon de Bruyn <pomp.enteral.06@icloud.com>
2025-10-11 08:43:19 +0200

    update: implement layout styling

--- app/src/app/app.ts
+++ app/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {VideoSection} from './components/video-section/video-section';
+import {ScheduledVideos} from './components/scheduled-videos/scheduled-videos';
+import {AvailableVideos} from './components/available-videos/available-videos';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [VideoSection, ScheduledVideos, AvailableVideos],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('app');
+
 }
