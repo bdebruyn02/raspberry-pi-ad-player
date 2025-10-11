d8b8256e0b531bc8717026a32ff5f6b199534966
Brandon de Bruyn <pomp.enteral.06@icloud.com>
2025-10-11 09:04:33 +0200

    add: available video styling

--- app/src/app/components/available-videos/available-videos.ts
+++ app/src/app/components/available-videos/available-videos.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import {IVideo} from '../../interfaces/video';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-available-videos',
-  imports: [],
+  imports: [MatListModule, MatButtonModule, MatIconModule],
   templateUrl: './available-videos.html',
   styleUrl: './available-videos.scss'
 })
 export class AvailableVideos {
+  list: IVideo[] = [];
 
 }
